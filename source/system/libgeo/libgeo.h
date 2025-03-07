82e9f123e3a89689089e217cc9774cf134947c04
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-03-07 10:56:52 -0500

    Create libgeo.h

---
+++ source/system/libgeo/libgeo.h
@@ -0,0 +1,14 @@
+#ifndef __MGS3_LIBGEO_H__
+#define __MGS3_LIBGEO_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* geod.c */
+void GEO_StartDaemon( void );
+
+#ifdef __cplusplus
+}
+#endif
+#endif // {{{ END OF FILE }}}
