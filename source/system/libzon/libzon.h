fd6a130502f5aead2b88b35c128ea9edab007e99
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-03-07 10:57:11 -0500

    Create libzon.h

---
+++ source/system/libzon/libzon.h
@@ -0,0 +1,17 @@
+#ifndef __MGS3_LIBZON_H__
+#define __MGS3_LIBZON_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+//
+//
+//
+//
+
+#ifdef __cplusplus
+}
+#endif
+#endif // {{{ END OF FILE }}}
