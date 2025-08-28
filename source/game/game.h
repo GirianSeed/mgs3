6e4fb9b6d2bc32034587e9b3363bb440d6afa6f0
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-08-27 23:04:34 -0400

    Create game.h

---
+++ source/game/game.h
@@ -0,0 +1,14 @@
+#ifndef __MGS3_GAME_H__
+#define __MGS3_GAME_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* gamed.c */
+void GM_StartDaemon( void );
+
+#ifdef __cplusplus
+}
+#endif
+#endif // {{{ END OF FILE }}}
