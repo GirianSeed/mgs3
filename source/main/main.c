af23568a7ff88a86f2425294403025353436712e
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-06-04 12:07:15 -0400

    Update main.c

--- source/main/main.c
+++ source/main/main.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <eekernel.h>
 #include <sifrpc.h>
@@ -21,22 +22,28 @@
 extern void MTS_BootThread( char *name, void (*func)(), int pri,
                 void *stack_top, int stack_size, void *arg );
 
+/*---------------------------------------------------------------------------*/
 // local to main/main.c
+
 extern int main_argc;
 extern char **main_argv;
-extern int main_thid;
+extern int boot_th_id;
 
-extern char main_stack[0x4000];
+#define STACK_SIZE (16 * 1024)
+
+extern u_long128 main_stack[ STACK_SIZE / sizeof(u_long128) ];
 extern void Main();
+
+/*---------------------------------------------------------------------------*/
 
 int main( int argc, char *argv[] )
 {
     main_argc = argc;
     main_argv = argv;
-    main_thid = GetThreadId();
+    boot_th_id = GetThreadId();
 
-    MTS_BootThread( "MGS/MAIN", Main, 18, main_stack, 0x4000, NULL );
-    while (1) {
+    MTS_BootThread( "MGS/MAIN", Main, 18, main_stack, STACK_SIZE, NULL );
+    for (;;) {
         SleepThread();
     }
 }
